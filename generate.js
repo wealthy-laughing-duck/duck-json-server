5e2108c02aabb145f89b8fe9ac5600e60c489ecf
Tomasz Ducin <tomasz.ducin@gmail.com>
2016-07-23 16:15:31 +0200

    updated contract, improved fake data generation

--- generate.js
+++ generate.js
@@ -3,21 +3,51 @@ var jsf = require('json-schema-faker');
 
 var schemaDir = 'duck-raml/schemas';
 var collections = {
-  "users": "user-collection",
-  "incomes": "income-collection",
-  "outcomes": "income-collection"
+  "users": {
+    "file": "user-collection.json",
+    "minItems": 2,
+    "maxItems": 4
+  },
+  "incomeCategories": {
+    "file": "category-collection.json",
+    "minItems": 5,
+    "maxItems": 10
+  },
+  "outcomeCategories": {
+    "file": "category-collection.json",
+    "minItems": 10,
+    "maxItems": 30
+  },
+  "incomes": {
+    "file": "income-collection.json",
+    "minItems": 200,
+    "maxItems": 500
+  },
+  "outcomes": {
+    "file": "income-collection.json",
+    "minItems": 1000,
+    "maxItems": 3000
+  }
 };
 
 var outputFile = "dist/db.json";
 var outputData = {};
 var promises = [];
 
+function jsonClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
 Object.keys(collections).forEach((collectionName) => {
+  var collectionData = collections[collectionName];
   var dfd = Promise.defer();
   promises.push(dfd.promise);
-  var path = schemaDir + "/" + collections[collectionName] + ".json";
-  jsonfile.readFile(path, function(err, jsonSchema) {
-    var tmpCollection = jsf(jsonSchema);
+  var path = schemaDir + "/" + collectionData.file;
+  jsonfile.readFile(path, function(err, originalJSONSchema) {
+    var JSONSchema = jsonClone(originalJSONSchema);
+    JSONSchema.minItems = collectionData.minItems;
+    JSONSchema.maxItems = collectionData.maxItems;
+    var tmpCollection = jsf(JSONSchema);
     tmpCollection.forEach((el, ind) => {
       el.id = ind + 1;
     });
